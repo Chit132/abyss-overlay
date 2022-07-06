37ca7a6a5ed1b775f7c8b79f790a3186eb2a5b3f
Chit <61895718+Chit132@users.noreply.github.com>
2022-07-05 23:04:38 -0400

    lunar fix 2

--- src/index.js
+++ src/index.js
@@ -557,7 +557,23 @@ function main(event){
     currentWindow = remote.BrowserWindow.getAllWindows(); currentWindow = currentWindow[0];
     $('.clientbutton').css('display', 'none'); $('.startup').css('display', 'none'); $('#titles').css('display', 'block'); $('#indexdiv').css('display', 'block'); $('.tabsbuttons').css('display', 'inline-block'); $('#show').css('display', 'inline-block');
 
-    if (event.data.client === 'lunar'){logpath = config.get('lunarlog', `${homedir}/.lunarclient/logs/launcher/renderer.log`); $('#clientimg').attr('src', 'https://img.icons8.com/nolan/2x/lunar-client.png'); $('#clientimg').css({'height': '34px', 'top': '0px'});}
+    if (event.data.client === 'lunar'){
+        $('#clientimg').attr('src', 'https://img.icons8.com/nolan/2x/lunar-client.png'); $('#clientimg').css({'height': '34px', 'top': '0px'});
+        logpath = config.get('lunarlog', -1);
+        if (logpath === -1) {
+            let log_18 = { path: `${homedir}/.lunarclient/offline/1.8/logs/latest.log`, exists: false };
+            let log_189 = { path: `${homedir}/.lunarclient/offline/1.8.9/logs/latest.log`, exists: false };
+            log_18.exists = fs.existsSync(log_18.path);
+            log_189.exists = fs.existsSync(log_189.path);
+            if (log_18.exists && log_189.exists) {
+                log_18.modified = fs.statSync(log_18.path).mtime;
+                log_189.modified = fs.statSync(log_189.path).mtime;
+                logpath = log_18.modified > log_189.modified ? log_18.path : log_189.path;
+            }
+            else if (log_18.exists) logpath = log_18.path;
+            else logpath = log_189.path;
+        }
+    }
     else if (process.platform === 'darwin'){
         if (event.data.client === 'badlion'){logpath = config.get('badlionlog', `${homedir}/Library/Application Support/minecraft/logs/blclient/minecraft/latest.log`); $('#clientimg').attr('src', 'https://www.badlion.net/static/assets/images/logos/badlion-logo.png');}
         else if (event.data.client === 'vanilla'){logpath = config.get('vanillalog', `${homedir}/Library/Application Support/minecraft/logs/latest.log`); $('#clientimg').attr('src', 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png/revision/latest?cb=20190525093706');}
